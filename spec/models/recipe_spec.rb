d5905a32cc12fa39bd7904400ad83c75a17b3af2
Dico Diaz <dicodiaz@gmail.com>
2023-06-27 11:11:15 -0500

    Add unit tests for model

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Recipe, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { User.create(name: 'user name', email: 'user_email@mail.com', password: 'user_password') }
+  let(:recipe) { Recipe.create(name: 'recipe name', description: 'recipe description', user:) }
+
+  context 'validations' do
+    it('should be valid') do
+      expect(recipe).to be_valid
+    end
+
+    it 'should be invalid when name is not present' do
+      recipe.name = nil
+      expect(recipe).not_to be_valid
+    end
+
+    it 'should be invalid when description is not present' do
+      recipe.description = nil
+      expect(recipe).not_to be_valid
+    end
+  end
 end
