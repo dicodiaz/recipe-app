1fa6afda3e2aa3bd4af069b811b81881e94ff97d
Dico Diaz <dicodiaz@gmail.com>
2023-06-26 19:54:02 -0500

    remove recipe #edit and #update

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
+  root to: redirect('/foods', status: 302)
   resources :recipe_foods
-  resources :recipes
-  resources :foods, except: [:edit, :update]
+  resources :recipes, except: %i[edit update]
+  resources :foods, except: %i[edit update]
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # get '/foods/show', to: 'foods#show', as: 'show_food'
-  # get '/foods/index', to: 'foods#index'
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
