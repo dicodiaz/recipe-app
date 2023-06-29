0e0aeaf37a36dfcded3fe067d6606e15f81d22dc
Jaime Villegas <jaimevillegas296@gmail.com>
2023-06-29 16:04:37 -0500

    Add general shopping list controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   root to: redirect('/foods', status: 302)
   resources :recipe_foods
+  resources :general_shopping_list, only: %i[index]
   resources :recipes, except: %i[edit update]
   resources :foods, except: %i[edit update]
   devise_for :users
 
   get '/public_recipes', to: 'recipes#public_recipes'
-  get '/general_shopping_list', to: 'recipe_foods#index'
-
 end
