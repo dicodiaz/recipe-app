12f34077f41ea5ab713775a0b7a79e049a3e5511
Dico Diaz <dicodiaz@gmail.com>
2023-06-29 05:25:04 -0500

    Add models specs

--- spec/models/recipe_food_spec.rb
+++ spec/models/recipe_food_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe RecipeFood, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { User.create(name: 'user name', email: 'user_email@mail.com', password: 'user_password') }
+  let(:recipe) { Recipe.create(name: 'recipe name', description: 'recipe description', user:) }
+  let(:food) { Food.create(name: 'food name', measurement_unit: 'Grams', price: 2.0, quantity: 3, user:) }
+  let(:recipe_food) { RecipeFood.create(quantity: 2, recipe:, food:) }
+
+  context '#measured_quantity' do
+    it "should return a string with the recipe_food's quantity and the food's measurement unit" do
+      expect(recipe_food.measured_quantity).to eq('2 Grams')
+    end
+  end
+
+  context '#value' do
+    it "should return a string with the total price of the recipe_food's quantity" do
+      expect(recipe_food.value).to eq('$4.0')
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { User.create(name: 'user name', email: 'user_email@mail.com', password: 'user_password') }
+
+  context '#select_foods' do
+    before(:each) do
+      @food = Food.create(name: 'food name', measurement_unit: 'Grams', price: 2.0, quantity: 3, user:)
+      @food2 = Food.create(name: 'food2 name', measurement_unit: 'Units', price: 3.0, quantity: 2, user:)
+    end
+
+    it "should return an array with array elements, where each element includes each user's food's information" do
+      expect(user.select_foods).to eq([['food name (Grams)', @food.id], ['food2 name (Units)', @food2.id]])
+    end
+  end
 end
